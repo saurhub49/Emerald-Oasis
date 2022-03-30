ca6b30171e3b3c247338fa4f6a11f1b9db0741a5
D3_NaitikRathor_56406 <rathourn755@gmail.com>
2022-03-30 13:07:27 +0530

    foodItem decoration done

--- naitik_rathor/react-app/src/components/FoodItems/foodItem.js
+++ naitik_rathor/react-app/src/components/FoodItems/foodItem.js
@@ -7,17 +7,35 @@ const FoodItem = (props) => {
 
     return (
         <div className="food-tab">
-            {foodItem.name}
-            {foodItem.price}
+            <div className="food-cont">
+                <div className="food-image-tab">
+                    <img className="image" src={foodItem.image}></img>
+                </div>
+                <div className="txt-cont">
+                    <div class="row">
+                        <span className="col title">{foodItem.name}</span>
+                        <span className="col price">Price: ${foodItem.price}</span>
+                    </div>
+
+                    <div className="desc">
+                        {foodItem.description}
+                    </div>
+                </div>
+            </div>
+            <button className="add-cart">Add to Cart</button>
         </div>
     )
 
 
-    // return <div className="row">
-    //     <div className="col-4 food-tab">
-    //         <center className="food-image">
+    // return <div className="test">
+    //     <div className="food-tab">
+    //         <center className="container food-image">
     //             <img src={foodItem.image}></img>
     //         </center>
+    //         <div className="row">
+    //         <div className="col">{foodItem.name}</div>
+    //         <div className="col">$. {foodItem.price}</div>
+    //         </div>
     //     </div>
     // </div>
 }
